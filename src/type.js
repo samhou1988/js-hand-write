ebaf7590879dfe04f0dd774595d85bc438fadd80
samhou1988 <samhou1988@gmail.com>
2022-12-11 09:47:46 +0800

    type

---
+++ src/type.js
@@ -0,0 +1,12 @@
+!(function (_) {
+  "Boolean Number String Function Array Date RegExp Object Error".split(" ").forEach(type => {
+    _[`is${type}`] = (data) => {
+      return Object.prototype.toString.call(data) === "[object " + type + "]";
+    }
+  })
+})(_ = {});
+
+console.log(_.isBoolean(true))
+console.log(_.isNumber(`1`))
+console.log(_.isRegExp(/test/igm))
+console.log(_.isDate(new Date('2022-02-31')))
