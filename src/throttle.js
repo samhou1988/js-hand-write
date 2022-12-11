5af3a29e39f2d3bc71d250ac3071152f5151a8c2
samhou1988 <samhou1988@gmail.com>
2022-12-11 17:30:02 +0800

    throttle

--- src/throttle.js
+++ src/throttle.js
@@ -11,3 +11,17 @@ function throttle(callback, delay = 300) {
   }
 }
 
+// // delta
+// const throttle = function (func, delay) {
+//   let startTime = Date.now()
+
+//   return function (...args) {
+//     let lastTime = Date.now()
+
+//     if (lastTime - startTime > delay) {
+//       func.apply(this, args)
+//       startTime = Date.now()
+//     }
+//   }
+// }
+
