2d1329dfc2de85ef0cbcb7fdd11c747f3e5c7bf1
samhou1988 <samhou1988@gmail.com>
2022-12-11 16:53:28 +0800

    bind

--- src/bind.js
+++ src/bind.js
@@ -26,3 +26,39 @@ function callMe(city) {
 var callBinded = callMe.bind(me, 'Beijing');
 // Hi! my name is sam and my age is 34 and my city is Beijing and state is haidian
 callBinded('haidian')
+
+// Function.prototype.bind2 = function (context, ...args) {
+//   if (typeof this !== 'function') {
+//     throw new TypeError('Bind must be called on a function')
+//   }
+
+//   const executeBound = function (sourceFunc, boundFunc, context, callingContext, args) {
+//     if (!(callingContext instanceof boundFunc)) {
+//       // 如果调用方式不是new func的形式就直接调用sourceFunc，并且给到对应的参数即可
+//       return sourceFunc.apply(context, args)
+//     } else {
+//       // 类似于执行new的几个过程
+//       const self = Object.create(sourceFunc.prototype) // 处理new调用的形式
+//       const result = sourceFunc.apply(self, args)
+//       // 判断函数执行后的返回结果 非对象函数，则返回self
+//       if (result && typeof result === 'object' || typeof result === 'function') {
+//         return result
+//       } else {
+//         return self
+//       }
+//     }
+//   }
+//   const func = this
+
+//   const bound = function (...innerArgs) {
+//     return executeBound(func, bound, context, this, args.concat(innerArgs))
+//   }
+
+//   return bound
+// }
+
+// // 测试
+// // 1. 普通调用
+// const showName = function (sex, age) {
+//   console.log(this, sex, age)
+// }
